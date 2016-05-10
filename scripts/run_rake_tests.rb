00fbea1a4d6de11433c8859eea01539218cab57a
Roy Roque <Roy Roque>
2016-05-10 16:04:48 -0400

    [Roy] updated rakefile and script/run_rake script

--- scripts/run_rake_tests.rb
+++ scripts/run_rake_tests.rb
@@ -1,26 +1,25 @@
 puts "-----Starting Automation Test Execution-----"
 
-component = ''	
-spec_dir=File.expand_path(File.dirname(__FILE__) + "/../spec/")
-log_dir=File.expand_path(File.dirname(__FILE__) + "/../logs/")
 rake_bin_dir="C:/Ruby21-x64/bin"
+component = ''    ;#demo, smoke, wizard, etc,...
+
+log_dir=File.expand_path(File.dirname(__FILE__) + "/../logs/")
 rakefile_dir=File.expand_path(File.dirname(__FILE__) + "/../")
 curr_dir=Dir.pwd
 
-
 #########################################################################
 #########################################################################
-### Cleanup (if any) old logs found
-if Dir.glob("#{log_dir}/*.log").size > 0 
-  puts "Deleting old all logfiles found in #{log_dir} folder"
+### Cleanup (if any) old logs/html found
+Dir.glob("#{log_dir}/*.log").each do |f| 
+  File.delete(f)
+end
 
-  Dir.glob("#{log_dir}/*.log").each do |f| 
-    File.delete(f)
-  end
+Dir.glob("#{log_dir}/*.html").each do |f| 
+  File.delete(f)
 end
 
 ### Run RAKE
-Dir.chdir spec_dir
+Dir.chdir rakefile_dir
 if component == ''
   %x(#{rake_bin_dir}/rake)
 else 
@@ -29,12 +28,15 @@ end
 Dir.chdir curr_dir
 
 ### Check Logs for ERRORS
-if Dir.glob("#{log_dir}/*.log").size > 0 
+#if Dir.glob("#{log_dir}/*.log").size > 0 
+if Dir.glob("#{log_dir}/*.html").size > 0 
   result = []
 
-  Dir.glob("#{log_dir}/*.log").each do |logname|
-    puts "Checking log for status: '#{logname}'"
-    if desc = open(logname).grep(/Failure\/Error/)
+  #Dir.glob("#{log_dir}/*.log").each do |logname|
+  Dir.glob("#{log_dir}/*.html").each do |logname|
+    puts "Checking log/html for status: '#{logname}'"
+    #if desc = open(logname).grep(/Failure\/Error/)
+    if desc = open(logname).grep(/failed_spec_name/)
       unless desc.empty?
         err_msg = "Found error(s) on #{File.basename(logname)} : #{desc}"
         result << err_msg.to_s
@@ -42,7 +44,7 @@ if Dir.glob("#{log_dir}/*.log").size > 0
     end
   end
   unless result.flatten.empty?
-    puts "-----Errors/Failures encountered - check logs for more info-----"
+    puts "-----Errors/Failures encountered - check logs/html for more info-----"
     puts result.flatten
     exit 1
   else
