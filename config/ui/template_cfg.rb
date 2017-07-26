6e38c988f334b22112ebc25c49f277f153f4485d
Roy ROQUE <rroque@provenir.com>
2017-07-26 09:03:09 -0400

    Update template_cfg.rb

--- config/ui/template_cfg.rb
+++ config/ui/template_cfg.rb
@@ -3,6 +3,12 @@ module TemplateConfig
   def initialize_instance_variables(tc='default')
     @timenow = Time.now.strftime('%m%d_%H%M')
     
+    unless ENV['URL'].nil?
+      @rest_obj[:host]=ENV['URL']
+      @rest_obj[:port]=ENV['PORT']
+    else 
+      @template=YAML.load_file(File.expand_path(File.dirname(__FILE__)+"/../../config/profiles.yml"))["auto1"]
+    end
     @template=YAML.load_file(File.expand_path(File.dirname(__FILE__)+"/yml/template.yml"))["auto1"]
   end
   
