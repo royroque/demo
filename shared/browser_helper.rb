419d9f1b90dcf3b480c324c0d4c7c23ea26bafca
Roy ROQUE <rroque@provenir.com>
2017-10-05 13:08:28 -0400

    Update browser_helper.rb

--- shared/browser_helper.rb
+++ shared/browser_helper.rb
@@ -113,7 +113,7 @@ module BrowserHelper
   
   def take_screenshot
     puts __method__
-    Dir.mkdir ('../logs') unless File.exists?('../logs')
+    Dir.mkdir (File.expand_path(File.dirname(__FILE__)+"/../logs")) unless File.exists?(File.expand_path(File.dirname(__FILE__)+"/../logs"))
     filename = File.expand_path(File.dirname(__FILE__)+"/../logs/#{Time.now.strftime('%m%d%y_%H%M%S')}.png")
     browser.screenshot.save filename
     sleep 1
